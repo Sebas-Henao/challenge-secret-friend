d7eb9ba45bd01f2e269c4aa78e3bff5f1cc29c2a
sebas <sebashenao25@hotmail.com>
2025-02-21 10:58:57 -0500

    lógica de función agregarAmigo

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+const amigos    = [];
+
+let vaciarCampo = () => {
+    document.getElementById('amigo').value = '';
+};
+
+let agregarAmigo    = () => {
+    let nombre      = document.getElementById('amigo').value.trim();
+    nombre.trim() === '' ? alert('Por favor, inserte un nombre.') : amigos.push(nombre);
+    
+    console.log(amigos);                                         //eliminar luego es para debugging
+
+    let listado       = document.getElementById('listaAmigos');
+    listado.innerHTML = ''; 
+
+    for (let i = 0; i < amigos.length; i++) {
+        let li         = document.createElement('li');
+        li.textContent = amigos[i];
+        listado.appendChild(li);
+    }
+
+    vaciarCampo();
+
+    return;
+};
+
+
