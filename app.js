3302ccc4384d43762c17cf48af037ae9ac955ebb
sebas <sebashenao25@hotmail.com>
2025-02-21 15:06:57 -0500

    lógica de función sortear amigo y ajustes al código general

--- app.js
+++ app.js
@@ -1,28 +1,61 @@
 
-const amigos    = [];
+const amigos = [];
 
-let vaciarCampo = () => {
-    document.getElementById('amigo').value = '';
+let vaciarCampo         = (id) => {
+    let campo           = document.getElementById(id);
+    campo ? campo.value = '' : console.warn(`Campo con id: "${id}" No encontrado.`)
+};
+
+let vaciarLista = (id) => {
+    let listado                 = document.getElementById(id);
+    listado ? listado.innerHTML = '' : console.warn(`Campo con id: "${id}" No encontrado.`);
 };
 
 let agregarAmigo    = () => {
     let nombre      = document.getElementById('amigo').value.trim();
     nombre.trim() === '' ? alert('Por favor, inserte un nombre.') : amigos.push(nombre);
-    
-    console.log(amigos);                                         //eliminar luego es para debugging
 
-    let listado       = document.getElementById('listaAmigos');
-    listado.innerHTML = ''; 
+    vaciarLista('listaAmigos');
+
+    let listado     = document.getElementById('listaAmigos');
 
     for (let i = 0; i < amigos.length; i++) {
         let li         = document.createElement('li');
         li.textContent = amigos[i];
         listado.appendChild(li);
-    }
+    };
 
-    vaciarCampo();
-
+    vaciarCampo('amigo');
+    vaciarLista('resultado');
     return;
 };
 
+let sortearAmigo        = () => {
+    if (amigos.length === 0) {
+        alert('¡Ya no quedan amigos por sortear!');
 
+        return;
+    }
+
+    let numMax          = amigos.length;
+    let numeroAleatorio = Math.floor(Math.random() * numMax);
+    let resultado       = amigos.splice(numeroAleatorio, 1)[0];
+    let nombreSorteo    = document.getElementById('resultado');
+
+    vaciarLista('resultado');
+
+    let creacionNombre         = document.createElement('li');
+    creacionNombre.textContent = `Tu amigo secreto es: ¡${resultado}!`;
+    nombreSorteo.appendChild(creacionNombre);
+
+    let listado = document.getElementById('listaAmigos');
+
+    for (let i = 0; i < amigos.length; i++) {
+        let li = document.createElement('li');
+        li.textContent = amigos[i];
+        listado.appendChild(li);
+    };
+
+    vaciarLista('listaAmigos');
+
+};
